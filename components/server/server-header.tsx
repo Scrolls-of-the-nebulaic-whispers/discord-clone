9d9eb0f29bb3a8520b2ba110916cee5fe08b83bb
Vadims Sapotniks <vadims.sapotniks@loadero.com>
2023-10-07 20:02:54 +0300

    Create server sidebar

---
+++ components/server/server-header.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { ServerWithMembers } from "@/types";
+import { MemberRole } from "@prisma/client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  TrashIcon,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+interface Props {
+  server: ServerWithMembers;
+  role?: MemberRole;
+}
+
+export const ServerHeader = ({ server, role }: Props) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <button
+          className="w-full text-md font-semibold px-3 flex items-center h-12
+        border-neutral-200 dark:border-neutral-800 border-b-2
+        hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+        >
+          {server.name}
+
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-56 text-xs font-medium text-black
+      dark:text-neutral-400 space-y-[2px]"
+      >
+        {(isAdmin || isModerator) && (
+          <DropdownMenuItem
+            className="text-indigo-600 dark:text-indigo-400
+          px-3 py-2 text-sm cursor-pointer"
+          >
+            Invite people
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Manage Members
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {(isAdmin || isModerator) && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Create Channel
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {(isAdmin || isModerator) && <DropdownMenuSeparator />}
+
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <TrashIcon className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {!isAdmin && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,57 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { Server } from "@prisma/client";
+import { ServerHeader } from "./server-header";
+interface Props {
+  serverId: string;
+}
+
+export const ServerSidebar = async ({ serverId }: Props) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server: any = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const textChannels = server?.channels.filter(
+    (ch) => ch.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.channels.filter(
+    (ch) => ch.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channels.filter(
+    (ch) => ch.type === ChannelType.VIDEO
+  );
+  const members = server?.members.filter((m) => m.profileId !== profile.id);
+  const role = server?.members.find((m) => m.profileId === profile.id)?.role;
+
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
+      <ServerHeader server={server} role={role} />
+    </div>
+  );
+};
